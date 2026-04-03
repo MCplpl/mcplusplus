51eebed6422be2730e51d6e4c80043f53aa05f3a
biskasarchaniotakis <sussyos@protonmail.com>
2026-04-03 13:16:03 +0300

    init

---
+++ src/main/java/com/george/mcplusplus/MCClient.java
@@ -0,0 +1,10 @@
+package com.george.mcplusplus;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MCClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
